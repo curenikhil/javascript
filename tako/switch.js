e93e517adf73dee9a94a73a7d1c435ec03900a94
curenikhil <nikburry@gmail.com>
2025-03-10 11:56:55 +0530

    tutorial after 6 hour and 52min

---
+++ tako/switch.js
@@ -0,0 +1,23 @@
+let month = "march"
+switch (month) {
+    case "jan":
+        console.log("jan")
+        break;
+    
+    case "jan":
+        console.log("jan")
+        break;
+
+    case "feb":
+        console.log("feb")
+        break;
+
+    case "march":
+        console.log("march")
+        break;
+
+
+    default:
+        console.log("this is default")
+        break;
+}
