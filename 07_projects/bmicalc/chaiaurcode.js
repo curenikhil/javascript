6a164c275dcfacf8822e384d412f59b24bc1186a
curenikhil <nikburry@gmail.com>
2025-04-24 19:55:51 +0530

    file renamed

---
+++ 07_projects/bmicalc/chaiaurcode.js
@@ -0,0 +1,27 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault(); // Prevent form submission & page reload
+
+  const height = parseInt(document.querySelector('#height').value);
+  const weight = parseInt(document.querySelector('#weight').value);
+  const results = document.querySelector('#results');
+
+  if (isNaN(height) || height <= 0) {
+    results.innerText = 'Please enter a valid height';
+  } else if (isNaN(weight) || weight <= 0) {
+    results.innerText = 'Please enter a valid weight';
+  } else {
+    const bmi = (weight / (height / 100) ** 2).toFixed(2);
+    let category;
+
+    if (bmi < 18.6) {
+      category = 'Underweight';
+    } else if (bmi >= 18.6 && bmi <= 24.9) {
+      category = 'Normal weight';
+    } else {
+      category = 'Overweight';
+    }
+    results.innerText = `Your BMI is ${bmi} and status is ${category}`;
+  }
+});
---
+++ 07_projects/colorChanger/chaiaurcode.js
@@ -0,0 +1,52 @@
+// // document.getElementById('grey')
+// const buttons = document.querySelectorAll('.button')
+// const body = document.querySelector('body')
+
+// buttons.forEach( function(button){
+//   // console.log(button);
+//   button.addEventListener('click', function(e){
+//     // console.log(e)
+//     // console.log(e.target)
+
+//     if(e.target.id === 'grey'){
+//       body.style.backgroundColor = e.target.id
+//     }
+
+//     if(e.target.id === 'white'){
+//       body.style.backgroundColor = e.target.id
+//     }
+//     if(e.target.id === 'blue'){
+//       body.style.backgroundColor = e.target.id
+//     }
+
+//     if(e.target.id === 'yellow'){
+//       body.style.backgroundColor = e.target.id
+//     }
+//   })
+// });
+
+const buttons = document.querySelectorAll(".button");
+const body = document.querySelector("body");
+
+buttons.forEach(button  => {
+  button.addEventListener("click", function(e) {
+    body.style.backgroundColor = e.target.id
+  })
+})
+
+// buttons.forEach(function (button) {
+//   button.addEventListener("click", function (e) {
+//     if (e.target.id === "grey") {
+//       body.style.backgroundColor = e.target.id;
+//     }
+//     if (e.target.id === "white") {
+//       body.style.backgroundColor = e.target.id;
+//     }
+//     if (e.target.id === "yellow") {
+//       body.style.backgroundColor = e.target.id;
+//     }
+//     if (e.target.id === "blue") {
+//       body.style.backgroundColor = e.target.id;
+//     }
+//   });
+// });
